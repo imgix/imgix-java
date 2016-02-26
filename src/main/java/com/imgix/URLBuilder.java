4646c814dd656dcc357ce3da00660c5215948fc9
Paul Straw <paulstraw@paulstraw.com>
2016-02-25 16:49:47 -0800

    Bump version to 1.1.0

--- src/main/java/com/imgix/URLBuilder.java
+++ src/main/java/com/imgix/URLBuilder.java
@@ -6,7 +6,7 @@ import java.util.zip.CRC32;
 
 public class URLBuilder {
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 
 	public enum ShardStrategy {
 		CRC,
