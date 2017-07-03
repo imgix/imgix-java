fb8e0109a8566190c2f8769fe15ab80f5f7b9bac
Chris Zacharias <christopher.zacharias@gmail.com>
2017-07-03 11:29:26 -0700

    Fixing a version number.

--- src/main/java/com/imgix/URLBuilder.java
+++ src/main/java/com/imgix/URLBuilder.java
@@ -6,7 +6,7 @@ import java.util.zip.CRC32;
 
 public class URLBuilder {
 
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "1.1.10";
 
 	public enum ShardStrategy {
 		CRC,
