e936db7da63fab3cc13334eacf31aa53ad628893
Sherwin H <sherwin@imgix.com>
2019-06-12 14:31:53 -0700

    fix: remove deprecated domain sharding functionality

--- src/main/java/com/imgix/URLBuilder.java
+++ src/main/java/com/imgix/URLBuilder.java
@@ -2,93 +2,39 @@ package com.imgix;
 
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.zip.CRC32;
 
 public class URLBuilder {
 
 	public static final String VERSION = "1.2.0";
 
-	public enum ShardStrategy {
-		CRC,
-		CYCLE
-	}
-
-	private String[] domains;
+	private String domain;
 	private boolean useHttps;
 	private String signKey;
-	private ShardStrategy shardStrategy;
 	private boolean signWithLibraryParameter;
 
-	private int shardCycleNextIndex = 0;
+	public URLBuilder(String domain, boolean useHttps, String signKey, boolean signWithLibraryParameter) {
 
-	/**
-	 * @deprecated  As of 1.2.0, domain sharding has been deprecated
-	 * 				and will be removed in the next major version.
-	 */
-	@Deprecated
-	public URLBuilder(String[] domains, boolean useHttps, String signKey, ShardStrategy shardStrategy, boolean signWithLibraryParameter) {
-
-		if (domains == null || domains.length == 0) {
+		if (domain == null || domain.length() == 0) {
 			throw new IllegalArgumentException("At lease one domain must be passed to URLBuilder");
 		}
 
-		this.domains = domains;
+		this.domain = domain;
 		this.useHttps = useHttps;
 		this.signKey = signKey;
-		this.shardStrategy = shardStrategy;
 		this.signWithLibraryParameter = signWithLibraryParameter;
 	}
 
 	public URLBuilder(String domain) {
-		this(new String[] {domain}, false);
-	}
-
-	/**
-	 * @deprecated  As of 1.2.0, domain sharding has been deprecated
-	 * 				and will be removed in the next major version.
-	 */
-	@Deprecated
-	public URLBuilder(String[] domain) {
 		this(domain, false);
 	}
 
-	/**
-	 * @deprecated  As of 1.2.0, domain sharding has been deprecated
-	 * 				and will be removed in the next major version.
-	 */
-	@Deprecated
-	public URLBuilder(String[] domain, boolean useHttps) {
+	public URLBuilder(String domain, boolean useHttps) {
 		this(domain, useHttps, "");
 	}
 
-	public URLBuilder(String domain, boolean useHttps) {
-		this(new String[] {domain}, useHttps, "");
-	}
-
-	/**
-	 * @deprecated  As of 1.2.0, domain sharding has been deprecated
-	 * 				and will be removed in the next major version.
-	 */
-	@Deprecated
-	public URLBuilder(String[] domain, boolean useHttps, String signKey) {
-		this(domain, useHttps, signKey, ShardStrategy.CRC, true);
-	}
 
 	public URLBuilder(String domain, boolean useHttps, String signKey) {
-		this(new String[] {domain}, useHttps, signKey, ShardStrategy.CRC, true);
-	}
-
-	public URLBuilder(String domain, boolean useHttps, String signKey, ShardStrategy shardStrategy) {
-		this(new String[] {domain}, useHttps, signKey, shardStrategy, true);
-	}
-
-	/**
-	 * @deprecated  As of 1.2.0, domain sharding has been deprecated
-	 * 				and will be removed in the next major version.
-	 */
-	@Deprecated
-	public void setShardStratgy(ShardStrategy strat) {
-		shardStrategy = strat;
+		this(domain, useHttps, signKey, true);
 	}
 
 	public void setUseHttps(boolean useHttps) {
@@ -105,20 +51,6 @@ public class URLBuilder {
 
 	public String createURL(String path, Map<String, String> params) {
 		String scheme = this.useHttps ? "https": "http";
-
-		String domain;
-
-		if (shardStrategy == ShardStrategy.CRC) {
-			CRC32 c = new CRC32();
-			c.update(path.getBytes());
-			int index = (int) (c.getValue() % domains.length);
-			domain = domains[index];
-		} else if (shardStrategy == ShardStrategy.CYCLE) {
-			shardCycleNextIndex = (shardCycleNextIndex + 1) % domains.length;
-			domain = domains[shardCycleNextIndex];
-		} else {
-			domain = domains[0];
-		}
 
 		if (this.signWithLibraryParameter) {
 			params.put("ixlib", "java-" + VERSION);
