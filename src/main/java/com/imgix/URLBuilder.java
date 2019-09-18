282ea0299ff08d4ab31799ecc47e27b3d64984f3
Sherwin H <sherwin@imgix.com>
2019-09-17 23:12:11 -0400

    feat: add domain validation

--- src/main/java/com/imgix/URLBuilder.java
+++ src/main/java/com/imgix/URLBuilder.java
@@ -2,10 +2,12 @@ package com.imgix;
 
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Pattern;
 
 public class URLBuilder {
 
 	public static final String VERSION = "2.0.0";
+	private static final String DOMAIN_REGEX = "^(?:[a-z\\d\\-_]{1,62}\\.){0,125}(?:[a-z\\d](?:\\-(?=\\-*[a-z\\d])|[a-z]|\\d){0,62}\\.)[a-z\\d]{1,63}$";
 
 	private String domain;
 	private boolean useHttps;
@@ -13,9 +15,12 @@ public class URLBuilder {
 	private boolean includeLibraryParam;
 
 	public URLBuilder(String domain, boolean useHttps, String signKey, boolean includeLibraryParam) {
+		Pattern domainPattern = Pattern.compile(DOMAIN_REGEX);
 
 		if (domain == null || domain.length() == 0) {
 			throw new IllegalArgumentException("At lease one domain must be passed to URLBuilder");
+		} else if (!domainPattern.matcher(domain).matches()) {
+			throw new IllegalArgumentException("Domain must be passed in as fully-qualified domain name and should not include a protocol or any path element, i.e. \"example.imgix.net\".");
 		}
 
 		this.domain = domain;
